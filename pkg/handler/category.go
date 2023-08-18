6ff2319455ee7c1cb2fed7a1f25e3e55bb926931
Nguyễn Xuân Vũ <xvung126@gmail.com>
2023-08-18 13:48:40 +0700

    update-api-sending-api-to-queue

--- pkg/handler/category.go
+++ pkg/handler/category.go
@@ -4,10 +4,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/hibiken/asynq"
 	"gitlab.com/merakilab9/j4/pkg/model"
 	"gitlab.com/merakilab9/j4/pkg/service"
+	"gitlab.com/merakilab9/j4/pkg/utils"
 	"gitlab.com/merakilab9/meracore/ginext"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -60,6 +63,41 @@ func (h *CateHandlers) GetUrlCate(c *ginext.Request) (*ginext.Response, error) {
 		return nil, err
 	}
 	fmt.Println(string(body))
+
+	return ginext.NewResponseData(http.StatusOK, rs), nil
+
+}
+
+func (h *CateHandlers) SendAPIToQueue(c *ginext.Request) (*ginext.Response, error) {
+
+	client := asynq.NewClient(asynq.RedisClientOpt{Addr: utils.RedisAddr})
+	defer client.Close()
+
+	//=================== MssBroker ===================//
+
+	rs, err := h.service.GetUrlCate()
+	if err != nil {
+		return nil, err
+	}
+
+	data4tune := model.RequestData{
+		rs,
+	}
+
+	payload4tune, err := json.Marshal(data4tune)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	task := asynq.NewTask(utils.APICateDelivery, payload4tune)
+	log.Printf(" Create tasks: %v", err)
+
+	info, err := client.Enqueue(task)
+	if err != nil {
+		log.Fatalf("could not enqueue tasks: %v", err)
+	}
+	log.Printf("enqueued tasks: id=%s queue=%s", info.ID, info.Queue)
 
 	return ginext.NewResponseData(http.StatusOK, rs), nil
 
--- pkg/route/route.go
+++ pkg/route/route.go
@@ -2,15 +2,12 @@ package route
 
 import (
 	"github.com/gin-contrib/pprof"
-	"github.com/hibiken/asynq"
 	"gitlab.com/merakilab9/j4/conf"
 	handlerJ4 "gitlab.com/merakilab9/j4/pkg/handler"
 	"gitlab.com/merakilab9/j4/pkg/repo/pg"
 	serviceJ4 "gitlab.com/merakilab9/j4/pkg/service"
-	"gitlab.com/merakilab9/j4/pkg/utils"
 	"gitlab.com/merakilab9/meracore/ginext"
 	"gitlab.com/merakilab9/meracore/service"
-	"log"
 )
 
 type Service struct {
@@ -32,7 +29,6 @@ func NewService() *Service {
 
 	cateService := serviceJ4.NewCateService(repoPG)
 	cateHandler := handlerJ4.NewCateHandlers(cateService)
-	cateHandlersQueue := handlerJ4.NewSendAPIToQueueHandlers(cateService)
 
 	shopIdService := serviceJ4.NewShopIdService(repoPG)
 	shopIdHandler := handlerJ4.NewShopIdHandlers(shopIdService)
@@ -46,28 +42,13 @@ func NewService() *Service {
 	v1Api := s.Router.Group("/api/v1")
 	v1Api.POST("/internal/migrate", migrateHandler.Migrate)
 
-	//v1Api.GET("/shopee/crawl-cate-url", ginext.WrapHandler(cateHandler.CrawlCate))
-
 	v1Api.GET("/shopee/get-cate-url", ginext.WrapHandler(cateHandler.GetUrlCate))
+
+	v1Api.GET("/shopee/send-api-to-queue", ginext.WrapHandler(cateHandler.SendAPIToQueue))
 
 	v1Api.GET("/shopee/get-shopid-url", ginext.WrapHandler(shopIdHandler.GetUrlShopId))
 
 	v1Api.GET("/shopee/get-item-url", ginext.WrapHandler(itemHandler.GetUrlItem))
-
-	client := asynq.NewClient(asynq.RedisClientOpt{Addr: utils.RedisAddr})
-	defer client.Close()
-	///=================== MssBroker ===================///
-
-	task, err := cateHandlersQueue.NewAPIDeliveryTask()
-	if err != nil {
-		log.Fatalf("could not create tasks: %v", err)
-	}
-
-	info, err := client.Enqueue(task)
-	if err != nil {
-		log.Fatalf("could not enqueue tasks: %v", err)
-	}
-	log.Printf("enqueued tasks: id=%s queue=%s", info.ID, info.Queue)
 
 	return s
 }
